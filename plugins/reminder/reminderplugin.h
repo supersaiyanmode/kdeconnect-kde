f4391365766b7606c3db2a1791e602f8ca766955
YANG Qiao <yangqiao0505@me.com>
2014-07-16 16:00:24 +0800

    add plugin reminder which would synchronize reminders(todo)

---
+++ plugins/reminder/reminderplugin.h
@@ -0,0 +1,77 @@
+/**
+ * Copyright 2014 YANG Qiao <yangqiao0505@me.com>
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License as
+ * published by the Free Software Foundation; either version 2 of
+ * the License or (at your option) version 3 or any later version
+ * accepted by the membership of KDE e.V. (or its successor approved
+ * by the membership of KDE e.V.), which shall act as a proxy
+ * defined in Section 14 of version 3 of the license.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#ifndef REMINDERPLUGIN_H
+#define REMINDERPLUGIN_H
+
+#include <QObject>
+
+#include <core/kdeconnectplugin.h>
+#include <Akonadi/Calendar/ETMCalendar>
+
+#define PACKAGE_TYPE_REMINDER QLatin1String("kdeconnect.reminder")
+
+class KDE_EXPORT ReminderPlugin
+    : public KdeConnectPlugin
+{
+    Q_OBJECT
+    Q_CLASSINFO("D-Bus Interface", "org.kde.kdeconnect.device.reminder")
+private:
+    typedef struct incidencInfo
+    {
+        QString uid;
+        QString summary;
+        QList<KDateTime> s_date;
+        QList<KDateTime> e_date;
+    }IncidenceInfo;
+	Akonadi::ETMCalendar* mCalendar;
+    QList<IncidenceInfo> mSentInfoList;
+    //TODO clean it up using overload of operators or create own classes to make it more structural
+    KCalCore::Incidence::Ptr itemToIncidence(const Akonadi::Item &item);
+    IncidenceInfo  incidenceToInfo(KCalCore::Incidence::Ptr& incidence);
+    bool incidenceIsIden(KCalCore::Incidence::Ptr& incidence1,KCalCore::Incidence::Ptr& incidence2);
+    bool incidenceIsIden(IncidenceInfo info1,IncidenceInfo info2);
+
+    bool calendarDidChanged();
+    void sendCalendar();
+    void addIncidence(KCalCore::Incidence::Ptr& incidence);
+    void modifyIncidence(KCalCore::Incidence::Ptr& incidence);
+    void deleteIncidence(KCalCore::Incidence::Ptr& incidence);
+    void deleteIncidence(QString& uid);
+    void mergeIncidence(KCalCore::Incidence::Ptr& incidence);
+    KCalCore::Incidence::Ptr parseICal(QString& iCal);
+
+public:
+    explicit ReminderPlugin(QObject *parent, const QVariantList &args);
+    virtual ~ReminderPlugin();
+
+public Q_SLOTS:
+    virtual bool receivePackage(const NetworkPackage& np);
+    virtual void connected();
+
+private slots:
+    void delayedRequest();
+    void calendarChanged();
+
+    void createFinished(bool success, const QString &errorMessage);
+    void deleteFinished(bool success, const QString &errorMessage);
+    void modifyFinished(bool success, const QString &errorMessage);
+};
+#endif
