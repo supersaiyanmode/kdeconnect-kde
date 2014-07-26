f6f651be0c570a9b986a73ea0d5519ceb4d2e0af
YANG Qiao <yangqiao0505@me.com>
2014-07-26 15:15:39 +0800

    use seperate akonadi resource for each device

--- plugins/calendar/calendarplugin.cpp
+++ plugins/calendar/calendarplugin.cpp
@@ -21,41 +21,40 @@
 #include "calendarplugin.h"
 #include <core/kdebugnamespace.h>
 
-#include <Akonadi/KCal/IncidenceMimeTypeVisitor>
+#include <Akonadi/AgentManager>
+#include <Akonadi/AgentInstanceCreateJob>
 #include <Akonadi/Calendar/IncidenceChanger>
+#include <Akonadi/CollectionFetchJob>
+#include <Akonadi/CollectionFetchScope>
 #include <Akonadi/KCal/IncidenceMimeTypeVisitor>
 #include <Akonadi/Collection>
 #include <Akonadi/Control>
 #include <KCalCore/Incidence>
 #include <KCalCore/ICalFormat>
+#include <KSharedConfig>
+#include <KConfigGroup>
+#include <QDBusConnection>
+#include <QDBusMessage>
 
 K_PLUGIN_FACTORY( KdeConnectPluginFactory, registerPlugin< CalendarPlugin >(); )
 K_EXPORT_PLUGIN( KdeConnectPluginFactory("kdeconnect_calendar", "kdeconnect-plugins") )
 
 CalendarPlugin::CalendarPlugin(QObject* parent, const QVariantList& args)
-    : KdeConnectPlugin(parent, args)
+    : KdeConnectPlugin(parent, args),mCalendar(QStringList()<<KCalCore::Event::eventMimeType())
 {
     if ( !Akonadi::Control::start() ) {
            kDebug(debugArea()) <<"Akonadi failed to start";
     }
     kDebug(debugArea()) << "Calendar plugin constructor for device" << device()->name();
-    Akonadi::IncidenceMimeTypeVisitor visitor;
-    QStringList mimelist;
-    mimelist<<visitor.eventMimeType();
-    mCalendar=new Akonadi::ETMCalendar(mimelist);
-    mCalendar->setCollectionFilteringEnabled(false);
-    connect(mCalendar,SIGNAL(createFinished(bool,QString)),SLOT(createFinished(bool,QString)));
-    connect(mCalendar,SIGNAL(modifyFinished(bool,QString)),SLOT(modifyFinished(bool,QString)));
-    connect(mCalendar,SIGNAL(deleteFinished(bool,QString)),SLOT(deleteFinished(bool,QString)));
-    connect(mCalendar,SIGNAL(calendarChanged()),SLOT(calendarChanged()));
+    setupResource();
+    connect(&mCalendar,SIGNAL(createFinished(bool,QString)),SLOT(createFinished(bool,QString)));
+    connect(&mCalendar,SIGNAL(modifyFinished(bool,QString)),SLOT(modifyFinished(bool,QString)));
+    connect(&mCalendar,SIGNAL(deleteFinished(bool,QString)),SLOT(deleteFinished(bool,QString)));
+    connect(&mCalendar,SIGNAL(calendarChanged()),SLOT(calendarChanged()));
 }
 
 CalendarPlugin::~CalendarPlugin()
 {
-    if (mCalendar){
-        delete(mCalendar);
-    }
-
     kDebug(debugArea()) << "Calendar plugin destructor for device" << device()->name();
 }
 
@@ -86,7 +85,7 @@ void CalendarPlugin::connected()
 
 void CalendarPlugin::delayedRequest()
 {
-    if (mCalendar->items().count()==0)
+    if (mCalendar.items().count()==0)
     {   
         NetworkPackage np(PACKAGE_TYPE_CALENDAR);
         np.set("request",true);
@@ -94,12 +93,77 @@ void CalendarPlugin::delayedRequest()
     }
 }
 
+int CalendarPlugin::setupResource()
+{
+    KSharedConfigPtr config = KSharedConfig::openConfig("kdeconnectrc");
+    KConfigGroup data = config->group("trusted_devices").group(device()->id());
+
+    mResourceId= data.readEntry<QString>("ical_resource_id", QLatin1String(""));
+    if (mResourceId!="")
+    {
+        kDebug(debugArea())<<" res id in config file"<<mResourceId;
+        Akonadi::AgentInstance mAgentInstance=Akonadi::AgentManager::self()->instance(mResourceId);
+        if (!mAgentInstance.isValid()){
+            mResourceId="";
+            kDebug(debugArea())<<"instance non valid";
+        }
+    }
+
+    if (mResourceId=="")
+    {
+        Akonadi::AgentInstanceCreateJob *job = new Akonadi::AgentInstanceCreateJob( "akonadi_icaldir_resource", this );
+        job->exec();
+        if ( job->error() ) {
+            kDebug(debugArea())<< "Create calendar failed:" << job->errorString();
+            return -1;
+        }
+        mResourceId=job->instance().identifier();
+        config->group("trusted_devices").group(device()->id()).writeEntry("ical_resource_id",mResourceId);
+        config->sync();
+        QDBusMessage msg = QDBusMessage::createMethodCall("org.freedesktop.Akonadi.Agent."+mResourceId, "/Settings", "org.kde.Akonadi.ICalDirectory.Settings", "setPath");
+        QList<QVariant> args;
+        args.append("kdeconnect_ical/"+device()->id());
+        msg.setArguments(args);
+        bool queued= QDBusConnection::sessionBus().send(msg);
+        job->instance().synchronize();
+    }
+    //wait a bit then fetch collection
+    mCalendar.setCollectionFilteringEnabled(false);
+    Akonadi::CollectionFetchJob *job = new Akonadi::CollectionFetchJob(Akonadi::Collection::root(),
+            Akonadi::CollectionFetchJob::Recursive,
+            this);
+    job->fetchScope().setContentMimeTypes(QStringList() <<  KCalCore::Event::eventMimeType());
+    job->setResource(mResourceId);
+    job->startTimer(100);
+    connect(job, SIGNAL(result(KJob*)), SLOT(collectionFetchResult(KJob*)));
+    return 0;
+}
+
+void CalendarPlugin::collectionFetchResult(KJob* job)
+{
+    Akonadi::CollectionFetchJob *fetchJob = static_cast<Akonadi::CollectionFetchJob*>( job );
+    if (fetchJob->collections().count()==0){
+        kDebug(debugArea())<<"no collection";
+    }else{
+        kDebug(debugArea())<<"got the collection";
+        mCollection=fetchJob->collections().first();
+    }
+    
+    if (mCollection.isValid()){
+        kDebug(debugArea())<<"collection fetch failed";
+        kDebug(debugArea())<<mCollection;
+    }
+    
+    mCalendar.incidenceChanger()->setDefaultCollection(mCollection);
+    mCalendar.incidenceChanger()->setDestinationPolicy(Akonadi::IncidenceChanger::DestinationPolicyNeverAsk);
+}
+
 void CalendarPlugin::sendCalendar()
 {
     if(!calendarDidChanged())
         return;
     //renew incidence list
-    Akonadi::Item::List newItemList=mCalendar->items();
+    Akonadi::Item::List newItemList=mCalendar.items(mCollection.id());
     mSentInfoList.clear();
     foreach(Akonadi::Item item, newItemList)
     {
@@ -109,7 +173,7 @@ void CalendarPlugin::sendCalendar()
 
     //send calendar
     KCalCore::ICalFormat ical;
-    foreach(Akonadi::Item item,mCalendar->items())
+    foreach(Akonadi::Item item,mCalendar.items(mCollection.id()))
     {
         KCalCore::Incidence::Ptr incidence=itemToIncidence(item);
         QList<KDateTime> datelist=incidence->startDateTimesForDate(QDate::currentDate(),KDateTime::LocalZone);
@@ -161,25 +225,25 @@ KCalCore::Incidence::Ptr CalendarPlugin::itemToIncidence(const Akonadi::Item &it
 void CalendarPlugin::addIncidence(KCalCore::Incidence::Ptr& incidence)
 {
     kDebug(debugArea())<<"add incidence"<<incidence->uid();
-    mCalendar->addIncidence(incidence);
+    mCalendar.addIncidence(incidence);
 }
 
 void CalendarPlugin::modifyIncidence(KCalCore::Incidence::Ptr& incidence)
 {
     kDebug(debugArea())<<"modify incidence"<<incidence->uid();
-    mCalendar->modifyIncidence(incidence);
+    mCalendar.modifyIncidence(incidence);
 }
 
 void CalendarPlugin::deleteIncidence(KCalCore::Incidence::Ptr& incidence)
 {
     kDebug(debugArea())<<"delete incidence"<<incidence->uid();
-    mCalendar->deleteIncidence(incidence);
+    mCalendar.deleteIncidence(incidence);
 }
 
 void CalendarPlugin::deleteIncidence(QString& uid)
 {
     kDebug(debugArea())<<"delete incidence"<<uid;
-    Akonadi::Item item=mCalendar->item(uid);
+    Akonadi::Item item=mCalendar.item(uid);
     if (item.isValid())
     {
         kDebug(debugArea())<<"item valid "<<uid;
@@ -196,7 +260,7 @@ void CalendarPlugin::mergeIncidence(KCalCore::Incidence::Ptr& incidence)
     kDebug(debugArea())<<"merge incidence"<<incidence->summary();
     //FIXME it's dangerous when receive two same incidence to add at the same time
     // a duplicate will occur
-    Akonadi::Item item=mCalendar->item(incidence->uid());
+    Akonadi::Item item=mCalendar.item(incidence->uid());
     if (item.isValid()){
         KCalCore::Incidence::Ptr oldInc=itemToIncidence(item);
         if (!incidenceIsIden(incidence,oldInc)){
@@ -213,7 +277,7 @@ void CalendarPlugin::mergeIncidence(KCalCore::Incidence::Ptr& incidence)
 
 bool CalendarPlugin::calendarDidChanged()
 {
-    Akonadi::Item::List newItemList=mCalendar->items();
+    Akonadi::Item::List newItemList=mCalendar.items(mCollection.id());
     if (newItemList.length()!=mSentInfoList.length()){
         kDebug(debugArea()) << "length not match";
         return true;
