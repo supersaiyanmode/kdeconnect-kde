0250779e97babdd2d29a3f347642e43aa541a110
Srivatsan Iyer <supersaiyanmode.rox@gmail.com>
2016-08-19 01:02:31 -0700

    WIP - POC. Add extremely basic KDE notification support.

---
+++ plugins/pyext/module.cpp
@@ -0,0 +1,25 @@
+#include <KNotification>
+
+#include "module.h"
+namespace {
+    PyObject* createSimpleNotification(PyObject* self, PyObject* text) {
+        PyObject* strObj = PyObject_Str(text);
+        const char* s = PyString_AsString(strObj);
+        
+        KNotification* notification = new KNotification("notification", KNotification::CloseOnTimeout);
+        notification->setIconName(QStringLiteral("preferences-desktop-notification"));
+        notification->setComponentName("kdeconnect");
+        notification->setTitle("Title");
+        notification->setText(QString(s));
+        notification->sendEvent();
+        
+        Py_DecRef(strObj);
+        
+        return NULL;
+    }
+}
+
+PyMethodDef module_exports[] = {
+    {"create_simple_notification", createSimpleNotification, METH_VARARGS, "docstring"},
+    {NULL, NULL, 0, NULL}
+};
---
+++ plugins/pyext/module.h
@@ -0,0 +1,8 @@
+#ifndef PYEXT_PYTHON_MODULE
+#define PYEXT_PYTHON_MODULE
+
+#include <Python.h>
+
+extern PyMethodDef module_exports[];
+
+#endif
